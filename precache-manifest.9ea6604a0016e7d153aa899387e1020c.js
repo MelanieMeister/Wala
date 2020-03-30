8452d95a89c46a2ecdb2ce60aa685fe021b6e72c
MelanieMeister <MelanieMeister>
2020-03-30 16:19:10 +0200

    Updates

---
+++ precache-manifest.9ea6604a0016e7d153aa899387e1020c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fc6c383fff912ccdefa72c8ae04aaf0",
+    "url": "/MelanieMeister/Wala.git/index.html"
+  },
+  {
+    "revision": "50e4589ba2018f7063c5",
+    "url": "/MelanieMeister/Wala.git/static/css/main.b3db496a.chunk.css"
+  },
+  {
+    "revision": "9d7c42dc2d0054d1ed70",
+    "url": "/MelanieMeister/Wala.git/static/js/2.524f2ac9.chunk.js"
+  },
+  {
+    "revision": "dcfd21bc398f724a8f83fe8b1e1a51ff",
+    "url": "/MelanieMeister/Wala.git/static/js/2.524f2ac9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e4589ba2018f7063c5",
+    "url": "/MelanieMeister/Wala.git/static/js/main.242f967e.chunk.js"
+  },
+  {
+    "revision": "20306c3f941e51a5cd01",
+    "url": "/MelanieMeister/Wala.git/static/js/runtime-main.c0d54aeb.js"
+  },
+  {
+    "revision": "4f786a64a71b61001733bfbd79ca3f2e",
+    "url": "/MelanieMeister/Wala.git/static/media/Layer.4f786a64.svg"
+  },
+  {
+    "revision": "4f4760b3ab20e361f53f9d4ccc453bfd",
+    "url": "/MelanieMeister/Wala.git/static/media/Logo.4f4760b3.svg"
+  },
+  {
+    "revision": "84bd80f1eeffbefe953b3c36d910e19c",
+    "url": "/MelanieMeister/Wala.git/static/media/Logo2.84bd80f1.png"
+  },
+  {
+    "revision": "692da9511da634a48c36c8a5e1320807",
+    "url": "/MelanieMeister/Wala.git/static/media/background.692da951.png"
+  },
+  {
+    "revision": "7d545b037698b2b4011bebdae2433d13",
+    "url": "/MelanieMeister/Wala.git/static/media/shop_background.7d545b03.png"
+  },
+  {
+    "revision": "c5735bb6362b31dd09470a4362240ddb",
+    "url": "/MelanieMeister/Wala.git/static/media/shop_logo.c5735bb6.png"
+  },
+  {
+    "revision": "9a21d26c757abfb777cdcb71e1172856",
+    "url": "/MelanieMeister/Wala.git/static/media/team_logo.9a21d26c.png"
+  }
+]);
